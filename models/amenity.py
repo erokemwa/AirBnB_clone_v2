8e5996e6802e5bc4d38d11447983ccf4e7645ee3
erokemwa <erokemwa@yahoo.com>
2023-03-24 10:09:30 -0700

    Amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-""" State Module for the HBNB project """
-from os import getenv
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """class for amenities"""
+    """A class that creates different amenities"""
     __tablename__ = 'amenities'
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity",
-                                       back_populates="amenities")
-    else:
-        name = ""
-
+    name = Column(String(128), nullable=False)
